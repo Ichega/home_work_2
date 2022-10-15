dae082c8634d7b0ed30e35ccec68b83c9f625935
Ichega <fristailik@gmail.com>
2022-10-16 02:35:48 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val ListOfNumbers = listOf("")
+    val ListOfValues = listOf<String>("value1" + "value2" +"value3" + "value4")
+    println(ListOfNumbers + ListOfValues - "")
+
+}
