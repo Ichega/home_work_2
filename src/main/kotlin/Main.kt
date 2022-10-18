a1cbd4973d8ae2c0ca5d10a1f42d05bc471da9a4
Ichega <112199724+Ichega@users.noreply.github.com>
2022-10-19 00:18:56 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 fun main() {
-    val ListOfNumbers = listOf("")
-    val ListOfValues = listOf<String>("value1" + "value2" +"value3" + "value4")
-    println(ListOfNumbers + ListOfValues - "")
+    var listOfStrings = arrayListOf<String>()
+    for (i in 0..20){
+        listOfStrings.add("Значение 1")
+    }
+println(listOfStrings)
 
 }
